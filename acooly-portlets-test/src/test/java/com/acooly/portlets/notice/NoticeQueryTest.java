29d039715d6458e622568f40b3559a8a4dd805de
zhangpu <zhangpu@acooly.cn>
2019-01-14 19:47:18 +0800

    调整test模块的依赖，fix单元测试启动错误问题。

--- acooly-portlets-test/src/test/java/com/acooly/portlets/notice/NoticeQueryTest.java
+++ acooly-portlets-test/src/test/java/com/acooly/portlets/notice/NoticeQueryTest.java
@@ -11,6 +11,7 @@ import com.acooly.portlets.notice.core.NoticeQueryService;
 import com.acooly.portlets.notice.core.entity.NoticeInfo;
 import com.acooly.portlets.notice.facade.dto.PageableNoticeInfo;
 import com.acooly.test.NoWebTestBase;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
@@ -24,38 +25,43 @@ import java.util.Map;
  */
 @Slf4j
 public class NoticeQueryTest extends NoWebTestBase {
-	
-	@Autowired
-	private NoticeQueryService noticeQueryService;
-	
-	
-	@Test
-	public void testReadNotice(){
-	
-	
-		
-	}
-	
-	@Test
-	public void testNoticePageQuery(){
-		Map<String,Object> params = new HashMap<> ();
-		
-		PageInfo pager = new PageInfo ();
-		
-		PageInfo<PageableNoticeInfo> pageInfo = noticeQueryService.pageQuery ("yanjun89", pager, params, null);
-		
-		log.info ("分页查询结果:{}",pageInfo);
-		
-	}
-	
-	@Test
-	public void testReadBroadcast(){
-		NoticeInfo noticeInfo = noticeQueryService.readNotice ("111111", 1L);
-		Assert.assertTrue (noticeInfo.getId ().equals (1) && noticeInfo.getReceiver ().equals ("ALL"));
-	}
-	
-	@Test
-	public void testReadAll(){
-		noticeQueryService.readAll ("yanjun89","liu");
-	}
+
+    @Autowired
+    private NoticeQueryService noticeQueryService;
+
+
+    @Test
+    public void testReadNotice() {
+
+
+    }
+
+    @Test
+    public void testNoticePageQuery() {
+        Map<String, Object> params = new HashMap<>();
+
+        PageInfo pager = new PageInfo();
+
+        PageInfo<PageableNoticeInfo> pageInfo = noticeQueryService.pageQuery("zhangpu1", pager, params, null);
+
+        log.info("分页查询结果:{}", JSON.toJSONString(pageInfo, true));
+
+    }
+
+    @Test
+    public void testUnReadCount() {
+        long unreadCount = noticeQueryService.countUnreadNotice("zhangpu1", null);
+        log.info("unreadCount:{}", unreadCount);
+    }
+
+    @Test
+    public void testReadBroadcast() {
+        NoticeInfo noticeInfo = noticeQueryService.readNotice("111111", 1L);
+        Assert.assertTrue(noticeInfo.getId().equals(1) && noticeInfo.getReceiver().equals("ALL"));
+    }
+
+    @Test
+    public void testReadAll() {
+        noticeQueryService.readAll("zhangpu1", null);
+    }
 }
--- acooly-portlets-test/src/test/java/com/acooly/portlets/notice/NoticeServiceTest.java
+++ acooly-portlets-test/src/test/java/com/acooly/portlets/notice/NoticeServiceTest.java
@@ -13,7 +13,6 @@ import com.acooly.portlets.notice.core.service.NoticeInfoService;
 import com.acooly.portlets.notice.facade.enums.DeviceTypeEnum;
 import com.acooly.test.NoWebTestBase;
 import com.google.common.collect.Lists;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -24,53 +23,53 @@ import java.util.Map;
  * Created by liubin@prosysoft.com on 2017/12/13.
  */
 public class NoticeServiceTest extends NoWebTestBase {
-	
-	
-	@Autowired
-	private NoticeService noticeService;
-	
-	@Autowired
-	private NoticeInfoService infoService;
-	
-	
-	@Test
-	public void testBroadcast () {
-		NoticeMessage noticeMessage = new NoticeMessage ();
-		noticeMessage.setPushNo (Ids.getDid ());
-		noticeMessage.setPush (false);
-		noticeMessage.setTitle ("测试广播");
-		noticeMessage.setContent ("这是广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播。");
-		noticeMessage.setPush (true);
-		
-		Map<String, Object> context = new HashMap<> ();
-		context.put ("customerId", "12312312312");
-		context.put ("userName", "刘斌");
-		context.put ("realName", "刘斌");
-		
-		noticeMessage.setContext (context);
-		
-		noticeService.broadcast (noticeMessage);
-	}
-	
-	@Test
-	public void testGroup () {
-		NoticeMessage noticeMessage = new NoticeMessage ();
-		noticeMessage.setPushNo (Ids.getDid ());
-		noticeMessage.setTitle ("测试消息");
-		noticeMessage.setContent ("这是消息、消息、消息。");
-		noticeMessage.setPush (true);
-		
-		Map<String, Object> context = new HashMap<> ();
-		context.put ("customerId", "12312312312");
-		context.put ("userName", "刘斌");
-		context.put ("realName", "刘斌");
-		
-		noticeMessage.setContext (context);
-		noticeMessage.setDeviceType (DeviceTypeEnum.ANDROID);
-		
-		noticeService.group (noticeMessage, Lists.newArrayList ("yanjun89"));
-		
-	}
-	
-	
+
+
+    @Autowired
+    private NoticeService noticeService;
+
+    @Autowired
+    private NoticeInfoService infoService;
+
+
+    @Test
+    public void testBroadcast() {
+        NoticeMessage noticeMessage = new NoticeMessage();
+        noticeMessage.setPushNo(Ids.getDid());
+        noticeMessage.setPush(false);
+        noticeMessage.setTitle("测试广播");
+        noticeMessage.setContent("这是广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播、广播。");
+        noticeMessage.setPush(false);
+
+        Map<String, Object> context = new HashMap<>();
+        context.put("customerId", "12312312312");
+        context.put("userName", "刘斌");
+        context.put("realName", "刘斌");
+
+        noticeMessage.setContext(context);
+
+        noticeService.broadcast(noticeMessage);
+    }
+
+    @Test
+    public void testGroup() {
+        NoticeMessage noticeMessage = new NoticeMessage();
+        noticeMessage.setPushNo(Ids.getDid());
+        noticeMessage.setTitle("测试群发消息");
+        noticeMessage.setContent("这是消息、消息、消息。");
+        noticeMessage.setPush(false);
+
+        Map<String, Object> context = new HashMap<>();
+        context.put("customerId", "12312312312");
+        context.put("userName", "刘斌");
+        context.put("realName", "刘斌");
+
+        noticeMessage.setContext(context);
+        noticeMessage.setDeviceType(DeviceTypeEnum.ANDROID);
+
+        noticeService.group(noticeMessage, Lists.newArrayList("zhangpu1", "zhangpu2", "zhangpu3", "zhangpu4"));
+
+    }
+
+
 }
