fe66e2ed65d20c8bff3a831f4cef58b835e75a61
zhangpu <zhangpu@prosysoft.com>
2019-02-27 14:37:23 +0800

    修改comment和feedback组件的初始化资源的图标和菜单名称

--- acooly-portlets-comment/acooly-portlets-comment-core/src/main/resources/META-INF/database/comment/mysql/init.sql
+++ acooly-portlets-comment/acooly-portlets-comment-core/src/main/resources/META-INF/database/comment/mysql/init.sql
@@ -1,11 +1,11 @@
-INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`) VALUES ('20190101',NULL ,'前端组件', 'MENU', '0', '2017-12-13 15:02:00', NULL, '1', 'icons-resource-fangdaozhuomian', NULL);
-INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`) VALUES('20190106', '20190101','评论组件', 'URL', '0', '2017-12-13 15:02:00', '/manage/portlets/comment/comment/index.html', '1','icons-resource-fangdaozhuomian', NULL);
+INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`)
+VALUES('20190102',NULL ,'评论组件', 'MENU', '0', '2017-12-13 15:02:00', NULL, '1', 'fa fa-commenting', NULL);
+INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`)
+VALUES('20190106', '20190102','评论记录', 'URL', '0', '2017-12-13 15:02:00', '/manage/portlets/comment/comment/index.html', '1','fa fa-commenting', NULL);
 INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`, `create_time`, `update_time`)
-VALUES
-	(20190107, 20190101, '评论操作日志', 'URL', 0, '2019-01-09 18:34:55', '/manage/portlets/comment/commentLog/index.html', 1,
-	'icons-resource-shouzhimingxi', NULL, '2019-01-09 18:34:55', '2019-01-10 16:16:43');
+VALUES(`20190107`, '20190102', '评论动作', 'URL', 0, '2019-01-09 18:34:55', '/manage/portlets/comment/commentLog/index.html', 1,'fa fa-circle-o',
+NULL, '2019-01-09 18:34:55', '2019-01-10 16:16:43');
 
-
-INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '20190101');
+INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '20190102');
 INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '20190106');
 INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '20190107');
--- acooly-portlets-feedback/acooly-portlets-feedback-core/src/main/resources/META-INF/database/feedback/mysql/init.sql
+++ acooly-portlets-feedback/acooly-portlets-feedback-core/src/main/resources/META-INF/database/feedback/mysql/init.sql
@@ -1,6 +1,7 @@
-INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`) VALUES ('20190101',NULL ,'前端组件', 'MENU', '0', '2017-12-13 15:02:00', NULL, '1', 'icons-resource-fangdaozhuomian', NULL);
-INSERT INTO `sys_resource`(`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`, `create_time`, `update_time`) VALUES (2019011501, 20190101, '反馈组件', 'URL', 0, '2019-01-14 22:24:33', '/manage/portlets/feedback/feedback/index.html', 1, 'icons-resource-admin', NULL, '2019-01-14 22:24:33', '2019-01-14 22:24:33');
-
+INSERT INTO `sys_resource` (`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`)
+VALUES ('20190101',NULL ,'反馈组件', 'MENU', '0', '2017-12-13 15:02:00', NULL, '1', 'fa fa-comments', NULL);
+INSERT INTO `sys_resource`(`ID`, `PARENTID`, `NAME`, `TYPE`, `SHOW_STATE`, `ORDER_TIME`, `VALUE`, `SHOW_MODE`, `ICON`, `DESCN`, `create_time`, `update_time`)
+VALUES (2019011501, 20190101, '反馈管理', 'URL', 0, '2019-01-14 22:24:33', '/manage/portlets/feedback/feedback/index.html', 1, 'fa fa-comments', NULL,'2019-01-14 22:24:33', '2019-01-14 22:24:33');
 
 INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '20190101');
 INSERT INTO `sys_role_resc` (`ROLE_ID`, `RESC_ID`) VALUES ('1', '2019011501');
