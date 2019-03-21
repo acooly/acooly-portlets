b013964042680129f125a3d4d9b3d7b4f2bb3416
zhangpu <zhangpu@prosysoft.com>
2019-03-22 02:13:04 +0800

    notice修正mybatis的包扫描缺失配置

--- acooly-portlets-notice/acooly-portlets-notice-core/src/main/java/com/acooly/portlets/notice/PortletNoticeAutoConfig.java
+++ acooly-portlets-notice/acooly-portlets-notice-core/src/main/java/com/acooly/portlets/notice/PortletNoticeAutoConfig.java
@@ -12,17 +12,17 @@ package com.acooly.portlets.notice;
 
 import com.acooly.core.common.dao.support.StandardDatabaseScriptIniter;
 import com.google.common.collect.Lists;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import static com.acooly.portlets.notice.PortletNoticeProperties.PREFIX;
-
 import java.util.List;
+
+import static com.acooly.portlets.notice.PortletNoticeProperties.PREFIX;
 
 ;
 
@@ -33,27 +33,28 @@ import java.util.List;
 @EnableConfigurationProperties({PortletNoticeProperties.class})
 @ConditionalOnProperty(value = PREFIX + ".enable", matchIfMissing = true)
 @ComponentScan(basePackages = {"com.acooly.portlets.notice"})
-public class PortletNoticeAutoConfig{
-	@Autowired
-	private PortletNoticeProperties noticeProperties;
-	
-	@Bean
-	public StandardDatabaseScriptIniter appScriptIniter () {
-		return new StandardDatabaseScriptIniter () {
-			@Override
-			public String getEvaluateTable () {
-				return "p_notice_info";
-			}
-			
-			@Override
-			public String getComponentName () {
-				return "notice";
-			}
-			
-			@Override
-			public List<String> getInitSqlFile () {
-				return Lists.newArrayList ("portlets_notice_init","portlets_notice_init_urls");
-			}
-		};
-	}
+@MapperScan("com.acooly.portlets.notice.core.dao")
+public class PortletNoticeAutoConfig {
+    @Autowired
+    private PortletNoticeProperties noticeProperties;
+
+    @Bean
+    public StandardDatabaseScriptIniter appScriptIniter() {
+        return new StandardDatabaseScriptIniter() {
+            @Override
+            public String getEvaluateTable() {
+                return "p_notice_info";
+            }
+
+            @Override
+            public String getComponentName() {
+                return "notice";
+            }
+
+            @Override
+            public List<String> getInitSqlFile() {
+                return Lists.newArrayList("portlets_notice_init", "portlets_notice_init_urls");
+            }
+        };
+    }
 }
