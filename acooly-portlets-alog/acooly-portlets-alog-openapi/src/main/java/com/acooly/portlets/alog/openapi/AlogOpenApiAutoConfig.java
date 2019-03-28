70f851bd27f394c1e6ec47ef7b585a9080363fe0
zhangpu <zhangpu@prosysoft.com>
2019-03-28 15:09:45 +0800

    fix: alog-openapi模块的启用和关闭通过acooly.alog.enable参数控制

--- acooly-portlets-alog/acooly-portlets-alog-openapi/src/main/java/com/acooly/portlets/alog/openapi/AlogOpenApiAutoConfig.java
+++ acooly-portlets-alog/acooly-portlets-alog-openapi/src/main/java/com/acooly/portlets/alog/openapi/AlogOpenApiAutoConfig.java
@@ -10,14 +10,21 @@
  */
 package com.acooly.portlets.alog.openapi;
 
+import com.acooly.portlets.alog.core.AlogProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import static com.acooly.portlets.alog.core.AlogProperties.PREFIX;
 
 
 /**
  * @author zhangpu
  */
 @Configuration
+@EnableConfigurationProperties({AlogProperties.class})
+@ConditionalOnProperty(value = PREFIX + ".enable", matchIfMissing = true)
 @ComponentScan(basePackages = {"com.acooly.portlets.alog.openapi"})
 public class AlogOpenApiAutoConfig {
 
