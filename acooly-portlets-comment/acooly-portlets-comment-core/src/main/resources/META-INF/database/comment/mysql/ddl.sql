4c85b08425b7cb485dab7b63777f6bfb69259e90
zhangpu <zhangpu@prosysoft.com>
2019-05-27 15:06:03 +0800

    提交comment_attach的表结构

--- acooly-portlets-comment/acooly-portlets-comment-core/src/main/resources/META-INF/database/comment/mysql/ddl.sql
+++ acooly-portlets-comment/acooly-portlets-comment-core/src/main/resources/META-INF/database/comment/mysql/ddl.sql
@@ -39,3 +39,16 @@ CREATE TABLE `p_comment_log` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='评论日志';
 
+CREATE TABLE `p_comment_attach` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `comment_id` bigint(20) NOT NULL COMMENT '评论ID',
+  `comment_no` varchar(64) NOT NULL COMMENT '评论编码',
+  `attach_type` varchar(32) NOT NULL COMMENT '附件类型 {photo:图片,video:视频,other:其他}',
+  `thumbnail` varchar(128) DEFAULT NULL COMMENT '缩略图',
+  `attach_path` varchar(128) NOT NULL COMMENT '附件地址',
+  `sort_time` bigint(20) DEFAULT NULL COMMENT '排序值',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `comments` varchar(128) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='评论附件';
